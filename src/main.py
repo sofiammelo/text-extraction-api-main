8d001316154424b35eca1c1b2a06da69b153f6cc
Sofia Melo <sofiamelo.br@gmail.com>
2026-03-18 12:40:26 +0000

    Ajuste no tratamento de erros do controller de extração

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from app.routes.ExtractionRoutes import extraction_routes
 
 app = Flask(__name__)
 
-# Registrem as rotas aqui, igual no exemplo abaixo.
 app.register_blueprint(example_routes)
 app.register_blueprint(extraction_routes)
 
